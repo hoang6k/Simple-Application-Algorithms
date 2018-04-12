340dbfd92f6eb75dcb6b947c3af6438afea24824
Hoàng Sáu Ka <38175475+hoang6k@users.noreply.github.com>
2018-04-12 11:57:45 +0700

    Create ADD.cpp

---
+++ ADD.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+/*    long long int a, b, c;
+    scanf("%lli %lli", &a, &b);
+    c = a + b;
+    printf("%lli", c);
+*/  
+    double a, b, c;
+    cin >> a >> b;
+    c = a + b;
+    printf("%.0f", c);
+    return 0;
+}

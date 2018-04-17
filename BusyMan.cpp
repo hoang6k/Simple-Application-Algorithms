1c7e1514e9cc345d24f9f6173ac8391d4510bbc6
Hoàng Sáu Ka <38175475+hoang6k@users.noreply.github.com>
2018-04-18 00:30:18 +0700

    Update BusyMan.cpp

--- BusyMan.cpp
+++ BusyMan.cpp
@@ -3,19 +3,19 @@ using namespace std;
 #define MAXT 10
 #define MAXN 100000
 int T;
-unsigned long N, start[MAXN] = { }, end[MAXN] = { }, ix[MAXN] = { },
-		result[MAXT] = { }, _max, _count;
+int N, start[MAXN] = { }, _end[MAXN] = { }, ix[MAXN] = { }, result[MAXT] = { },
+		_max, _count;
 template<typename _Tp>
 struct indexless: public binary_function<_Tp, _Tp, bool> {
 	bool operator()(const _Tp& _x, const _Tp& _y) const {
-		return end[_x] < end[_y];
+		return _end[_x] < _end[_y];
 	}
 };
-void busyman(unsigned long k) {
-	for (unsigned long i = k + 1; i <= N; i++)
-		if (start[ix[i]] >= end[ix[k]]) {
+void busyman(int k) {
+	for (int i = k + 1; i <= N; i++)
+		if (start[ix[i]] >= _end[ix[k]]) {
 			_count++;
-			_max = _max > _count ? _max : _count;
+			_max = max(_max, _count);
 			if (i < N)
 				busyman(i);
 			_count--;
@@ -25,18 +25,19 @@ int main() {
 	scanf("%d", &T);
 	int i = -1;
 	while (++i < T) {
-		_count = _max = 0;
-		scanf("%lu", &N);
-		for (unsigned long j = 1; j <= N; j++)
+		_count = 0;
+		_max = 0;
+		scanf("%d", &N);
+		for (int j = 1; j <= N; j++)
 			ix[j] = j;
-		for (unsigned long j = 1; j <= N; j++)
-			scanf("%lu%lu", &start[j], &end[j]);
-		sort(ix + 1, ix + N + 1, indexless<unsigned long>());
+		for (int j = 1; j <= N; j++)
+			scanf("%d %d", &start[j], &_end[j]);
+		sort(ix + 1, ix + N + 1, indexless<int>());
 		busyman(0);
 		result[i] = _max;
 	}
 	i = -1;
 	while (++i < T)
-		printf("%lu\n", result[i]);
+		printf("%d\n", result[i]);
 	return 0;
 }

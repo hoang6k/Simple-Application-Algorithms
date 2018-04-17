887ba122f71b226088b639db8c87b1944921f6b3
Hoàng Sáu Ka <38175475+hoang6k@users.noreply.github.com>
2018-04-18 02:36:35 +0700

    Update BusyMan.cpp

--- BusyMan.cpp
+++ BusyMan.cpp
@@ -3,58 +3,35 @@ using namespace std;
 #define MAXT 10
 #define MAXN 100000
 int T;
-int N, start[MAXN] = { }, _end[MAXN] = { }, result[MAXT] = { }, _max, _count;
-int partition(int low, int high) {
-	int pivot = _end[high];
-	int i = low - 1;
-	int temp;
-	for (int j = low; j <= high - 1; j++) {
-		if (_end[j] <= pivot) {
-			i++;
-			temp = _end[i];
-			_end[i] = _end[j];
-			_end[j] = temp;
-			temp = start[i];
-			start[i] = start[j];
-			start[j] = temp;
-		}
+int N, start[MAXN], _end[MAXN], ix[MAXN], result[MAXT], _count;
+//template<typename _Tp>
+struct indexless: public binary_function<int, int, bool> {
+	bool operator()(const int& _x, const int& _y) const {
+		return _end[_x] < _end[_y];
 	}
-	temp = _end[i + 1];
-	_end[i + 1] = _end[high];
-	_end[high] = temp;
-	temp = start[i + 1];
-	start[i + 1] = start[high];
-	start[high] = temp;
-	return (i + 1);
-}
-void quickSort(int low, int high) {
-	if (low < high) {
-		int pi = partition(low, high);
-		quickSort(low, pi - 1);
-		quickSort(pi + 1, high);
-	}
-}
+};
 void busyman(int k) {
-	for (int i = k + 1; i <= N; i++)
-		if (start[i] >= _end[k]) {
+	for (int i = k + 1; i < N; i++)
+		if (start[ix[i]] >= _end[ix[k]]) {
 			_count++;
-			_max = max(_max, _count);
-			if (i < N)
+			if (i < N - 1)
 				busyman(i);
-			_count--;
+			return;
 		}
 }
 int main() {
 	scanf("%d", &T);
 	int i = -1;
 	while (++i < T) {
-		_count = _max = 0;
+		_count = 1;
 		scanf("%d", &N);
-		for (int j = 1; j <= N; j++)
+		for (int j = 0; j < N; j++)
+			ix[j] = j;
+		for (int j = 0; j < N; j++)
 			scanf("%d %d", &start[j], &_end[j]);
-		quickSort(1, N);
+		sort(ix, ix + N, indexless());
 		busyman(0);
-		result[i] = _max;
+		result[i] = _count;
 	}
 	i = -1;
 	while (++i < T)
